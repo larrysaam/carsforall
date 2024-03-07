1b44c452d5372c0b86f1e61f31cb115b76636fae
larrysaam <nyaah.saam@ictuniversity.edu.cm>
2024-03-07 21:09:07 +0100

    styling

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,14 +1,18 @@
 import './header.css'
 import logo from '../../assets/logo2.png'
+import email from '../../assets/email.png'
+import call from '../../assets/call.png'
+import facebook from '../../assets/facebook.png'
+
 
 const Header = ()=>{
 
     return(
         <nav className="navbar">
             <div className='info_div'>
-                <button>650814245</button>
-                <button>nyaah73@gmail.com</button>
-                <button>Kamer Motors</button>
+                <button><img src={call} alt='.' id='top_icon'/>650814245</button>
+                <button><img src={email} alt='.' id='top_icon'/>nyaah73@gmail.com</button>
+                <button><img src={facebook} alt='.' id='top_icon'/>Kamer Motors</button>
             </div>
             <img
                 src={logo}
--- src/component/item/post.js
+++ src/component/item/post.js
@@ -21,14 +21,14 @@ const Post = ({data, made, model, images, price, age})=>{
                 />
             </div>
             <div className='item_list_details'>
-                <h4>Price: <span>{price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span></h4>
+                <h4>Prix: <span>{price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span></h4>
                 <div className='details_ul_div'>
                         <p> <b>Model: </b> {model}</p>
-                        <p> <b>Years: </b> {age}</p>
+                        <p> <b>age: </b> {age}</p>
                 </div>
                 <div className='btn_div'>
                     <button className='whatsapp_btn'  onClick={()=>{window.location.href = 'https://wa.link/70d550'}}><img src={logo} alt='.' id='whatsapp_icon'/>  Whatsapp </button>
-                    <button className='see_btn' onClick={()=>navigate('/article', {state: {data}})}><img src={seeicon} alt='.' className='see_more_img'/> See More </button>
+                    <button className='see_btn' onClick={()=>navigate('/article', {state: {data}})}><img src={seeicon} alt='.' className='see_more_img'/> Voir </button>
                 </div>
             </div>
         </div>
